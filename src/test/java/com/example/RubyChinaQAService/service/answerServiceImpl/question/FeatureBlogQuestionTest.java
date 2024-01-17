245ae96e39fae06a4cf950d7d4cce8f853364963
fusen he <111634076+wow2048@users.noreply.github.com>
2024-01-17 20:45:41 +0800

    Delete src/test/java/com/example/RubyChinaQAService/service/answerServiceImpl/question directory

--- src/test/java/com/example/RubyChinaQAService/service/answerServiceImpl/question/FeatureBlogQuestionTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.RubyChinaQAService.service.answerServiceImpl.question;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FeatureBlogQuestionTest {
-}
